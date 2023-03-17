451c4c49f3e560f3e2ce883d0dbaa382aff5550e
Vieira14N <ianvieira0411@gmail.com>
2023-03-17 16:39:34 -0300

    feat: função createLink em andamento

--- src/components/Shorten.js
+++ src/components/Shorten.js
@@ -2,6 +2,20 @@ import React from 'react'
 import '../styles/shorten.css'
 
 export default function Shorten() {
+  
+
+  function createLink() {
+    const container = document.querySelector('.link-container')
+    const link = document.createElement('div')
+    link.innerHTML = `<p>https://www.google.com</p>
+                      <div>
+                        <p>https://auaauhuau.bit</p>
+                        <button>Copy</button>
+                      </div>`
+    link.classList.add('link')
+    container.appendChild(link)
+  }
+
   return (
     <section className='shorten-section'>
       <div className='input-container'>
@@ -9,21 +23,18 @@ export default function Shorten() {
           type='text'
           placeholder='Shorten a link here...'
         ></input>
-        <button>Shorten it!</button>
+        <button onClick={createLink} >Shorten it!</button>
       </div>
       <div className='link-container'>
-        <p>https://www.google.com</p>
-        <div>
-          <p>https://auaauhuau.bit</p>
-          <button>Copy</button>
+
+        <div className='link'>
+          <p>https://www.google.com</p>
+          <div>
+            <p>https://auaauhuau.bit</p>
+            <button>Copy</button>
+          </div>
         </div>
-      </div>
-      <div className='link-container'>
-        <p>https://www.google.com</p>
-        <div>
-          <p>https://auaauhuau.bit</p>
-          <button>Copy</button>
-        </div>
+
       </div>
     </section>
   )
